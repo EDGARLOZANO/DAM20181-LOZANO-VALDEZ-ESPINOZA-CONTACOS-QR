d1f20eae74fed5fe05013fd5de49cda946789a2c
carlosE24 <eduardo_espinoza1@hotmail.com>
2018-05-31 19:18:03 -0600

    Scan Code

--- src/pages/crear-contacto/crear-contacto.ts
+++ src/pages/crear-contacto/crear-contacto.ts
@@ -36,7 +36,7 @@ public viewCtrl: ViewController,private barcodeScanner: BarcodeScanner) {
     this.activar=true;
   }
  
-  //Escandeo de codigo qr y llama al metodo para crear el contacto con los daros leidos  
+  //Escandeo de codigo qr y llama al metodo para crear el contacto con los datos leidos  
   scanCode() {
     this.barcodeScanner.scan().then(barcodeData => {
       this.scannedCode = barcodeData.text;
@@ -54,7 +54,29 @@ public viewCtrl: ViewController,private barcodeScanner: BarcodeScanner) {
    * Funsion para agregar un nuevo contacto a la lista de contactos del telefono.
    */
   crearContacto(){
- 
+    let contact: Contact = this.contacts.create();
+    let avatar ="/assets/imgs/logo.png";
+
+    this.valores=this.scannedCode.split(',');
+    contact.displayName =this.datos['nombre']  =this.valores[0];
+    contact.phoneNumbers = [new ContactField(this.datos['tiponumero']="Movil",
+    this.datos['numero']=this.valores[1])];
+    contact.photos = [new ContactField('url',avatar,false)]
+    contact.save().then(
+      () => { 
+        console.log('Contact Guardado!', contact)
+        this.dismiss({estado:true,contacto:contact});
+      },
+      (error: any) => {
+        console.error('Error al guardar el contacto.', error)
+        this.dismiss({estado:false});
+      }
+    );
+  }
+// para cancelar la creacion de contacto
+  dismiss(data) {
+    this.viewCtrl.dismiss(data);
+  }
 
 
 
